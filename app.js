880b2afcd58b03e1c6d0c7b36781f197bc8f7c55
qdong1982 <50020474+qdong1982@users.noreply.github.com>
2019-07-10 09:15:03 -0400

    change comment to app.js, make it neat

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-//jshint esversion:6
-
-const express = require ("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
@@ -8,68 +6,70 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/", function(req, res){
+
+app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
+
+  //This data you can get it from homepage form
   let firstName = req.body.firstName;
   let lastName = req.body.lastName;
   let email = req.body.email;
-let data = {
-  members: [
-    {email_address:email,
+
+  //Mailchimp API
+  let data = {
+    members: [{
+      email_address: email,
       status: "subscribed",
-      merge_fields:{
+      merge_fields: {
         FNAME: firstName,
         LNAME: lastName,
       }
-    }
-  ]
-};
+    }]
+  };
 
-let jsonData = JSON.stringify(data);
+  //turn javascript object to JSON
+  let jsonData = JSON.stringify(data);
 
-const options = {
-  url: 'https://us20.api.mailchimp.com/3.0/lists/941c982119',
-  method: "POST",
+  const options = {
+    //get from API require
+    url: 'https://us20.api.mailchimp.com/3.0/lists/941c982119',
+    method: "POST",
 
-//This is basic authorization
-  headers:{
-    "Authorization": "quang 78849142b55f3ce955d8de75d0a5e60d-us20"
-    //Any string plus your API key. This is for every API
-  },
+    //This is basic authorization
+    headers: {
+      "Authorization": "quang 78849142b55f3ce955d8de75d0a5e60d-us20"
+      //Any string plus your API key. This is for every API
+    },
 
-  body: jsonData
-};
+    body: jsonData
+  };
 
-request(options, function(error, response, body){
-  if (error){
-    res.sendFile(__dirname+ "/failure.html");
-
-  } else {
-    if (response.statusCode ===200){
-      res.sendFile(__dirname + "/success.html");
-    } else {
+  request(options, function (error, response, body) {
+    if (error) {
       res.sendFile(__dirname + "/failure.html");
+
+    } else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
     }
-  }
-});
+  });
 
 });
 
-app.post("/failure", function(req, res){
+app.post("/failure", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(process.env.PORT ||3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
-
-//API key
- // 78849142b55f3ce955d8de75d0a5e60d-us20
-
-// list id
-// 941c982119
